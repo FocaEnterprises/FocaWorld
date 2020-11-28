e2d7d799bdb503b3d3180662ffb7dde22c2e7cab
GiverPlay007 <play.giver.007@gmail.com>
2020-11-27 21:13:32 -0300

    Retorno JSON do gerador.

--- src/main/java/com/focasoft/beterraba/world/gen/Generator.java
+++ src/main/java/com/focasoft/beterraba/world/gen/Generator.java
@@ -11,11 +11,29 @@ public class Generator
     this.SEED = seed;
   }
   
-  public JSONObject generate(String name, int width, int height)
+  public JSONObject generate(String name, int w, int h)
   {
+    LevelGen.resetAndSetSeed(SEED);
     JSONObject json = new JSONObject();
+    JSONObject tiles = new JSONObject();
     
-    //TODO
+    byte[] gen = LevelGen.createAndValidateMap(w, h)[0];
+    
+    for(int x = 0; x < w; x++)
+    {
+      for(int y = 0; y < h; y++)
+      {
+        int i = x + y * w;
+        int tile = gen[i];
+        
+        tiles.put(String.valueOf(i), tile);
+      }
+    }
+    
+    json.put("name", name);
+    json.put("width", w);
+    json.put("height", h);
+    json.put("tiles", tiles);
     
     return json;
   }
