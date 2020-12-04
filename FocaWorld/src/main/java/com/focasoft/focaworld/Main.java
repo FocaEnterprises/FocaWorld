caee6caeb6effaa7ba30626e3aac366106b3ce72
GiverPlay007 <play.giver.007@gmail.com>
2020-12-04 10:44:03 -0300

    Adapt

--- FocaWorld/src/main/java/com/focasoft/focaworld/Main.java
+++ FocaWorld/src/main/java/com/focasoft/focaworld/Main.java
@@ -7,6 +7,13 @@ public class Main
 {
   public static void main(String[] args)
   {
+    if(args.length == 0)
+    {
+      System.out.println("No arguments, starting on default mode");
+      new Server("ForcedStart");
+      return;
+    }
+
     if(args.length < 2)
     {
       System.out.println("Missing arguments!");
